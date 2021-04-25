c602c854adb53d029b1efa2055f83d1d7958c616
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-04-25 16:04:10 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll                          long long
+#define mod                         1000000007
+#define f                           first
+#define s                           second
+#define pb                          push_back
+#define mk                          make_pair
+#define fast_io                     ios_base::sync_with_stdio(false);\
+                                    cin.tie(NULL)
+#define PI                          3.1415926535897932384626
+#define INF                         1e18
+#define EPS                         1e-9
+
+bool flg = false;
+
+const int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int dir8[8][2] = {{0, 1}, {0, -1}, {1, 0}, { -1, 0}, {1, 1}, {1, -1}, { -1, 1}, { -1, -1}};
+
+void pavan_coder()
+{
+
+}
+
+int main()
+{
+	fast_io;
+	ll tc = 1;
+	cin>>tc;
+	while(tc--)
+	{
+		pavan_coder();
+	}
+}
