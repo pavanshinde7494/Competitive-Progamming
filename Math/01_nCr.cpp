4af9b540b04adb8a4b51cf9f7c935edead1bd985
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-05-05 10:56:41 +0530

    Update 01_nCr.cpp

--- Math/01_nCr.cpp
+++ Math/01_nCr.cpp
@@ -8,3 +8,23 @@ ll nCr(ll n,ll r)
     return nCr(n-1,r) + nCr(n-1,r-1); 
 }   
 
+//Efficient Sol
+
+ll dp[1000];   
+
+
+ll nCr(ll n,ll r)
+{
+    if(r>n)
+        return 0;
+        
+    dp[0] = 1;
+    dp[1] = n;
+    for(ll i=2;i<=r;i++)
+    {
+        dp[i] = (dp[i-1]*(n-i+1))/i;
+    }
+    for(ll i=0;i<=r;i++)
+        cout<<dp[i]<<" ";
+    return dp[r];
+}   
