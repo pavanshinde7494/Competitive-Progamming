e2eccde0c651669bf3247ed4205aa657527253e5
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-05-05 10:52:19 +0530

    Update 02_nPr.cpp

--- Math/02_nPr.cpp
+++ Math/02_nPr.cpp
@@ -8,3 +8,22 @@ ll nPr(ll n,ll r)
     return nPr(n-1,r) + r*nPr(n-1,r-1); 
 }   
 
+//Efficient Sol
+
+ll dp[1000];   
+
+
+ll nPr(ll n,ll r)
+{
+    if(r>n)
+        return 0;
+        
+    dp[0] = 1;
+    for(ll i=1;i<=r;i++)
+    {
+        dp[i] = dp[i-1]*(n-i+1);
+    }
+    for(ll i=0;i<=r;i++)
+        cout<<dp[i]<<" ";
+    return dp[r];
+}   
