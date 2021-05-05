4a0cfb5dfed34d7c31ef79706fe4b998c2547172
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-05-05 11:05:27 +0530

    Update 03_catalan_no.cpp

--- Math/03_catalan_no.cpp
+++ Math/03_catalan_no.cpp
@@ -1,3 +1,12 @@
+// Applications of catalan no.
+	
+// 1.Count the number of expressions containing n pairs of parentheses which are correctly matched. 
+//   For n = 3, possible expressions are ((())), ()(()), ()()(), (())(), (()()).
+// 2.Count the number of possible Binary Search Trees with n keys (See this)
+// 3.Count the number of full binary trees (A rooted binary tree is full if every vertex has either two children or no children) with n+1 leaves.
+// 4.Given a number n, return the number of ways you can draw n chords in a circle with 2 x n points such that no 2 chords intersect.
+
+
 ll dp[1001];
     
 ll catalan(int n){
