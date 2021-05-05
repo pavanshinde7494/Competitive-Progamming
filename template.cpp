cbb909fe60da2f9b41da746595205b90e603e90c
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-05-05 09:37:23 +0530

    Update template.cpp
    Few changes

--- template.cpp
+++ template.cpp
@@ -8,21 +8,28 @@ using namespace std;
 #define s                           second
 #define pb                          push_back
 #define mk                          make_pair
+#define gcd(a,b)                    __gcd(a,b)
+#define lcm(a,b)                    a/gcd(a,b)*b
 #define fast_io                     ios_base::sync_with_stdio(false);\
                                     cin.tie(NULL)
 #define PI                          3.1415926535897932384626
 #define INF                         1e18
-#define EPS                         1e-9
+#define NINF                        -1e18
 
 bool flg = false;
 
 const int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
 const int dir8[8][2] = {{0, 1}, {0, -1}, {1, 0}, { -1, 0}, {1, 1}, {1, -1}, { -1, 1}, { -1, -1}};
+
+/*--------------------------------------------------------code starts here--------------------------------------------------------------*/
+
 
 void pavan_coder()
 {
 
 }
+
 
 int main()
 {
