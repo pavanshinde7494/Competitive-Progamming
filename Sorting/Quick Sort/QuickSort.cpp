d4ea5eefe4f5fda1ad847692a5817944442c4f2b
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-10-14 17:50:04 +0530

    quickSort

---
+++ Sorting/Quick Sort/QuickSort.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll                          long long
+#define mod                         1000000007
+#define f                           first
+#define s                           second
+#define pb                          push_back
+#define mk                          make_pair
+#define gcd(a,b)                    __gcd(a,b)
+#define lcm(a,b)                    a/gcd(a,b)*b
+#define fast_io                     ios_base::sync_with_stdio(false);\
+                                    cin.tie(NULL)
+#define PI                          3.1415926535897932384626
+#define INF                         1e18
+#define NINF                        -1e18
+
+bool flg = false;
+
+const int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, { -1, 0}};
+
+const int dir8[8][2] = {{0, 1}, {0, -1}, {1, 0}, { -1, 0}, {1, 1}, {1, -1}, { -1, 1}, { -1, -1}};
+
+/*--------------------------------------------------------code starts here--------------------------------------------------------------*/
+
+
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void quickSort(ll arr[], ll left , ll right) {
+    if (left >= right - 1)return;
+    ll pivote = arr[left];
+    ll pivoteIndex = left;
+    ll low = left + 1 , high = right - 1;
+
+    while (low < high) {
+        while (pivote >= arr[low] && low <= right )low++;
+        while (pivote < arr[high] && high >= left) high--;
+        if (low < high) {
+            swap(arr[low], arr[high]);
+        }
+        else
+            break;
+    }
+
+    swap(arr[pivoteIndex] , arr[high]);
+    pivoteIndex = high;
+
+    quickSort(arr , left , pivoteIndex);
+    quickSort(arr , pivoteIndex + 1 , right);
+}
+
+
+int main() {
+    fast_io;
+    file();
+    ll no;
+    cin >> no;
+
+    ll arr[no];
+    for (ll i = 0; i < no; i++)
+        cin >> arr[i];
+
+    quickSort(arr, 0, no);
+
+    for (ll i = 0; i < no; i++)
+        cout << arr[i] << " ";
+}
