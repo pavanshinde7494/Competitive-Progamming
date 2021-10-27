2f138d63bdf8b19deb994b27b4df73ca151693f3
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-10-27 14:53:15 +0530

    Create PostOrderItr.cpp

---
+++ Trees/PostOrderItr.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data structure to store a binary tree node
+struct Node
+{
+    int data;
+    Node *left, *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = nullptr;
+    }
+};
+
+// Iterative function to perform inorder traversal on the tree
+void inorderIterative(Node* root)
+{
+    Node* curr = root;
+    stack<Node*>stk;
+    stk.push(curr);
+    stack<int>out;
+
+    while (!stk.empty()) {
+        Node* curr = stk.top();
+        out.push(curr->data);
+        stk.pop();
+
+        if (curr->left) {
+            stk.push(curr->left);
+        }
+        if (curr->right) {
+            stk.push(curr->right);
+        }
+    }
+    while (!out.empty()) {
+        cout << out.top() << " ";
+        out.pop();
+    }
+}
+
+int main()
+{
+
+    /* Construct the following tree
+               1
+             /   \
+            /     \
+           2       3
+          /      /   \
+         /      /     \
+        4      5       6
+              / \
+             /   \
+            7     8
+    */
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    root->right->left->left = new Node(7);
+    root->right->left->right = new Node(8);
+
+    inorderIterative(root);
+
+    return 0;
+}
+
