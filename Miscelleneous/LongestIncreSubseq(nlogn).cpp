a34557b36c3cceeb42556c46abd33fce8b75999c
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-11-10 11:13:20 +0530

    Update LongestIncreSubseq(nlogn).cpp

--- Miscelleneous/LongestIncreSubseq(nlogn).cpp
+++ Miscelleneous/LongestIncreSubseq(nlogn).cpp
@@ -1,43 +1,57 @@
-int longestSubsequence(int n, int a[])
-    {
-       // your code here
-       vector<int>seq;
-       vector<int>index;
-       int track[n];
-       
-       memset(track,-1,sizeof(track));
-       
-       seq.push_back(a[0]);
-       
-       index.push_back(0);
-       
-       int lastIndex = 0;
-       
-       for(int i=1;i<n;i++)
-       {
-           if(a[i] > seq[seq.size()-1])
-           {
-               track[i] = index[index.size()-1];
-               seq.push_back(a[i]);
-               index.push_back(i);
-               lastIndex = i;
-           }
-           else
-           {
-               int place = lower_bound(seq.begin(),seq.end(),a[i]) - seq.begin();
-               seq[place] = a[i];
-               index[place] = i;
-               if(place != 0)
-                    track[i] = index[place-1];
-           }
-       }
-      
-       
-       while(lastIndex!=-1)
-       {
-            cout<<a[lastIndex]<<" ";
-            lastIndex = track[lastIndex];
-       }
-       cout<<"\n";
-       return seq.size();
+
+int LISnlogn(int arr[], int no) {
+    int len = 0;
+    int tmp[no] , track[no];
+
+    memset(tmp, -1, sizeof(tmp));
+    memset(track, -1, sizeof(track));
+
+    tmp[0] = 0;
+
+    for (int i = 1; i < no; i++) {
+        if (arr[i] >= arr[tmp[len]]) {
+            len++;
+            tmp[len] = i;
+            track[i] = tmp[len - 1];
+        }
+        else if (arr[i] < arr[tmp[0]]) {
+            tmp[0] = i;
+        }
+        else {
+            int left = 0 , right = len , res = -1;
+            while (left <=  right) {
+                int mid = (left + right) / 2;
+                if (arr[tmp[mid]] >= arr[i]) {
+                    res = mid;
+                    right = mid - 1;
+                }
+                else
+                    left = mid + 1;
+            }
+            tmp[res] = i;
+            track[i] = tmp[res - 1];
+        }
     }
+    for (int i = 0; i < no; i++)
+        cout << tmp[i] << " ";
+    cout << "\n";
+    for (int i = 0; i < no; i++)
+        cout << track[i] << " ";
+    cout << "\n";
+    return len + 1;
+}
+
+
+
+int main() {
+    fast_io;
+    file();
+    int no;
+    cin >> no;
+    int arr[no];
+    for (int i = 0; i < no; i++)
+        cin >> arr[i];
+
+    cout << LISnlogn(arr, no);
+    return 0;
+}
