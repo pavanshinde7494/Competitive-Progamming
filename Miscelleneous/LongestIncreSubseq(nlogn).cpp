b91f2425af42316d1f3afdc6a64637f9b7da0197
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-11-10 11:21:50 +0530

    Update LongestIncreSubseq(nlogn).cpp

--- Miscelleneous/LongestIncreSubseq(nlogn).cpp
+++ Miscelleneous/LongestIncreSubseq(nlogn).cpp
@@ -1,22 +1,38 @@
 
+
+
 int LISnlogn(int arr[], int no) {
     int len = 0;
-    int tmp[no] , track[no];
+
+    // tmp array gives best possible element for increasing subseq. of particular length
+    int tmp[no] ;
+    // Track array used to keep track of LIS
+    int track[no];
+    // both above arrays keeps indices not elements
+
 
     memset(tmp, -1, sizeof(tmp));
     memset(track, -1, sizeof(track));
 
+
     tmp[0] = 0;
 
     for (int i = 1; i < no; i++) {
+        // check curr ele is >= min possible ele of increasing subseq of length = len
         if (arr[i] >= arr[tmp[len]]) {
             len++;
             tmp[len] = i;
             track[i] = tmp[len - 1];
         }
+
+        // check curr ele is < min possible ele of increaing subseq of length = 0
         else if (arr[i] < arr[tmp[0]]) {
             tmp[0] = i;
         }
+
+        // if both above cases fails
+        // means curr ele should be placed between 0 and len
+        // curr ele should be replaced by is ceil
         else {
             int left = 0 , right = len , res = -1;
             while (left <=  right) {
