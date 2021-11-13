e1b14221bc2eb7053af32c033eec29e75373cb62
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2021-11-13 10:26:38 +0530

    Update 04Finding_articulation_pt.cpp

--- Graph Theory/04Finding_articulation_pt.cpp
+++ Graph Theory/04Finding_articulation_pt.cpp
@@ -1,59 +1,63 @@
 
 vector<int>adjList[1001];
-
-
-int articulation[1001];
 int visited[1001];
-int tin[1001];
 int low[1001];
+int tin[1001];
+int articulation[1001];
 int timer = 1;
 
-void findingArticulation(int node,int parent){
+void findArticulation(int node, int parent) {
     visited[node] = 1;
-    tin[node] = tin[node] = timer++;
+    low[node] = tin[node] = timer++;
 
     int childCnt = 0;
-    for(int child : visited[node]){
-        if(child == parent)continue;
-        if(visited[node])
-            low[node] = min(low[node],tin[child]);
-        else{
-            findingArticulation(child,node);
-            low[node] = min(low[node],low[child]);
-            if(low[child] >= tin[node] && parent != -1){
-                articulation[node] = 1;
-            }
+    for (int child : adjList[node]) {
+        if (child == parent)continue;
+        if (visited[child])
+            low[node] = min(low[node] , tin[child]);
+        else {
+            findArticulation(child, node);
+            low[node] = min(low[node], low[child]);
+            if (tin[node] <= low[child] && parent != -1)
+                articulation[node]  = 1;
             childCnt++;
         }
     }
-    if(parent == -1 && childCnt > 1)
+    if (childCnt > 1 && parent == -1)
         articulation[node] = 1;
 }
 
-int main(){
 
-    memset(visited,0,sizeof(visited));
-    memset(articulation,0,sizeof(articulation));
-    memset(low,0,sizeof(low));
-    memset(tin,0,sizeof(tin));
 
-    int nodes,edges;
-    cin>>nodes>>edges;
+int  main() {
+    fast_io;
+    file();
 
-    for(int i=1;i<=edges;i++){
-        int u,v;
-        cin>>u>>v;
+    memset(visited, 0, sizeof(visited));
+    memset(low, 0, sizeof(low));
+    memset(tin, 0, sizeof(tin));
+    memset(articulation, 0, sizeof(articulation));
+
+    int nodes, edges;
+    cin >> nodes >> edges;
+
+    for (int i = 0; i < edges; i++) {
+        int u, v;
+        cin >> u >> v;
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
 
-    for(int i=1;i<=nodes;i++){
-        if(!visited[i])
-            findingArticulation(i,-1);
+    for (int i = 1; i <= nodes; i++) {
+        if (!visited[i]) {
+            findArticulation(i, -1);
+        }
     }
-    cout<<"Articulation Points : ->\n";
-    for(int i=1;i<=nodes;i++){
-        if(articulation[i])
-            cout<<i<<" , ";
+
+    cout << "Articulation Points -> \n" ;
+    for (int i = 1; i <= nodes; i++) {
+        if (articulation[i])
+            cout << i << " , ";
     }
+    return 0;
 }
