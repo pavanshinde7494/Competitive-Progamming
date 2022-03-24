8fb4110f3424528b421b2a9a57db627a713a623e
pavanshinde7494 <64213894+pavanshinde7494@users.noreply.github.com>
2022-03-24 16:33:05 +0530

    Update 08krushkal_algo.cpp

--- Graph Theory/08krushkal_algo.cpp
+++ Graph Theory/08krushkal_algo.cpp
@@ -1,64 +1,38 @@
-#include<bits/stdc++.h>
+int parent[1000001];
 
-using namespace std;
 
-#define ll long long
-#define mod 1000000007
-#define f first
-#define s second
-#define pb push_back
-#define mk make_pair
-
-struct edge{
-    ll x;
-    ll y;
-    ll w;
-};
-
-edge e[100001];
-ll parent[10001];
-
-bool cmp(edge a,edge b)
-{
-    return a.w<b.w;
+bool cmp(vector<int>&p,vector<int>&q){
+  return (p[2] < q[2]);
 }
 
-void dsu_union(ll a,ll b)
-{
-    parent[b] = a;
+int findPar(int a){
+  if(parent[a] < 0)return a;
+  int tmp = findPar(parent[a]);
+  return tmp;
 }
 
-ll dsu_find(ll a)
-{
-    if(parent[a]<0)
-        return a;
-    ll tmp = dsu_find(parent[a]);
-    parent[a] = tmp;
-    return tmp;
+void makeUnion(int a ,int b){
+  parent[b] = a;
 }
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-    memset(parent,-1,sizeof(parent));
-    ll nodes,edges;
-    cin>>nodes>>edges;
-    for(ll i=0;i<edges;i++)
-    {
-        cin>>e[i].x>>e[i].y>>e[i].w;
-    }
-    sort(e,e+edges,cmp);
-    ll sum = 0;
-    for(ll i=0;i<edges;i++)
-    {
-        ll x_par = dsu_find(e[i].x);
-        ll y_par = dsu_find(e[i].y);
-        if(x_par != y_par)
-        {
-            sum += e[i].w;
-            dsu_union(x_par,y_par);
-        }
-    }
-    cout<<sum<<"\n";
+void krushkal(vector<vector<int>>edges){
+  // [
+  //   [x,y,w],
+  //   [x,y,w]
+  // ]
+  memset(parent,-1,sizeof(parent));
+  sort(edges.begin(),edges.end(),cmp); 
+
+  int mst = 0;
+  for(int i=0;i<edges.size();i++){
+      int x = edges[i][0];
+      int y = edges[i][1];
+      int x_par = findPar(x);
+      int y_par = findPar(y);
+      if(x_par != y_par){
+          mst += edges[i][2];
+          makeUnion(x_par,y_par);
+      }
+  }
+  cout<<mst<<"\n";
 }
